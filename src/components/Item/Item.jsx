608bb68051f7efccb7fb0acd891357e8786a9d0a
nayon-kumar <nayon.cse55@gmail.com>
2026-03-21 00:25:24 +0600

    Icon and line-through added

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Trash2 } from "lucide-react";
+import { Circle } from "lucide-react";
 import { CircleCheck } from "lucide-react";
 
 const Item = ({ id, text, isComplete, deleteTodo, toggle }) => {
@@ -9,10 +10,8 @@ const Item = ({ id, text, isComplete, deleteTodo, toggle }) => {
       className="flex items-center gap-3 justify-between border-2 rounded-lg p-5 border-gray-200"
     >
       <div className="flex items-center gap-3">
-        <div>
-          <CircleCheck />
-        </div>
-        <p>{text}</p>
+        <div>{isComplete ? <CircleCheck /> : <Circle />}</div>
+        <p className={isComplete ? "line-through" : ""}>{text}</p>
       </div>
       <div
         className="cursor-pointer text-red-500 p-0.5 rounded-full"
